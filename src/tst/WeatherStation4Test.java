d6a346c42bd3cd21ef40a6dd4fcc7e6dcd477b25
Benjamin Munoz <driver396@gmail.com>
2020-05-23 19:26:38 -0700

    Adjusted tests for Weather Station 4
    - Got rid of ss variable since its run() method had a while(true) {...}
    loop that prevented the rest of the test code from running.
    
    - Allowed time for the SensorSuite within ws4 to write data to
    output4.txt before the rest of the tests were run.

--- src/tst/WeatherStation4Test.java
+++ src/tst/WeatherStation4Test.java
@@ -11,6 +11,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
 
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,16 +20,24 @@ class WeatherStation4Test {
     WirelessConsole c;
     WeatherStationIntegrater in;
     WeatherStation4 ws4;
-    SensorSuite ss;
 
     @BeforeEach
     void setUp() throws Exception {
         c = new WirelessConsole();
         in = new WeatherStationIntegrater(c);
         ws4 = new WeatherStation4(in);
-        ss = new SensorSuite();
-        ss.run();
         ws4.run();
+
+        /*
+         * Give the SensorSuite contained in WeatherStation4 a few seconds 
+         * to write data to output4.txt
+         * 
+         * Otherwise, if an attempt is made to invoke the updateList()
+         * method too soon, a NoSuchElementException will be thrown since
+         * no information has been written to output4.txt.
+         */
+        var timeA = System.currentTimeMillis();
+        while (System.currentTimeMillis() - timeA <= 3000) {};
     }
 
     @Test
